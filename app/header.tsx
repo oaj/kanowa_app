5c8132927b41f99783fe524d7dfeebb6bbf011f1
Ole J <oaj@amfibia.dk>
2023-01-10 22:45:55 -0400

    global types

--- app/header.tsx
+++ app/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Header = () => {
     return (
-        <div className="flex gap-4 bg">
+        <div className="flex gap-4 bg-slate-400 font-extrabold underline">
             <Link href='/'>Home</Link>
             <Link href='/users'>Users</Link>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     // <h1>
     <h1 className="text-3xl font-bold underline text-red-500">
-      Home page, should be red.
+      Home page.
     </h1>
   )
 }
