4aa9e6c00b5dcdfbfad781d40d0391894d0b7d6b
Ole J <oaj@amfibia.dk>
2023-01-12 18:02:43 -0400

    css-menu

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,12 @@
+import { CssMenu } from '@/components/menu/CssMenu'
 import Link from 'next/link'
 
 export const Header = () => {
     return (
-        <div className="flex gap-4 bg-slate-400 font-extrabold underline">
-            <Link href='/'>Home</Link>
-            <Link href='/users'>Users</Link>
-        </div>
+        <CssMenu currentUser={undefined} />
+        // <div className="flex gap-4 bg-slate-400 font-extrabold underline">
+        //     <Link href='/'>Home</Link>
+        //     <Link href='/users'>Users</Link>
+        // </div>
     )
 }
